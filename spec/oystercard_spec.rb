cbb2109e11f1740e1f0c532ee03009f81e94a126
Kirk Smith <kirks@hotmail.co.uk>
2016-01-12 00:44:48 +0000

    added oystercard and balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,11 @@
+require 'oystercard'
+
 describe Oystercard do
-  
+
+  subject(:oystercard) {described_class.new}
+
+  it 'new card has balance' do
+    expect(oystercard.balance).to eq 0
+  end
+
 end
